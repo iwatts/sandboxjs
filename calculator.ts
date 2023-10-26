17192561abf4ba1260db0af566c8468f9a60e2cf
Isaac <theicean@gmail.com>
2023-10-26 12:06:17 -0600

    calc file

---
+++ calculator.ts
@@ -0,0 +1,52 @@
+let equationHistory: any[];
+let currentEquation: string;
+interface arithmaticOperator {
+    ADD: '+',
+    SUB: '-',
+    MULT: '*',
+    DIV: '/'
+};
+type solution = number|string;
+const currentInput = document.getElementById('currentInput') as HTMLInputElement | null;
+const previousInput = document.getElementById('previousInput') as HTMLInputElement | null;
+
+function isNumber(value) {
+  return typeof value === 'number';
+}
+
+function hasError(){
+    return 'ERR'
+}
+
+function getNumberFromInput(): solution{
+    let num: solution;
+    //get num from input
+    num = currentInput && currentInput.value ? Number(currentInput.value) : hasError();  
+    //set input val to empty
+    if (currentInput?.value) currentInput.value = '';
+    return num;
+}
+
+function composeFormula(action){
+    // Get input value from Input and clear the input
+    let inputVal: solution = getNumberFromInput();
+    if(isNumber(inputVal)) {
+        currentEquation += inputVal;
+        currentEquation += action;
+    } else {
+        hasError();
+    }
+}
+
+function solveFormula(): solution{
+    // Get input Value from Input and clear the input
+    let inputVal: solution = getNumberFromInput();
+
+    // add current val to history
+    currentEquation += inputVal;
+    equationHistory.push(currentEquation);
+    let answer = eval(currentEquation);
+
+    return answer;
+
+}
